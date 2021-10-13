fbdec5021f37341e362256b1105b810cba4e20a0
patrick story <pstory721@gmail.com>
2021-10-13 13:20:10 -0500

    venues post

--- backend/routes/api/create-venue.js
+++ backend/routes/api/create-venue.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const asyncHandler = require('express-async-handler');;
 const { User } = require('../../db/models');
-const { venue } = require('../../db/models');
+const { Venue } = require('../../db/models');
 const router = express.Router();
 
 router.get('/', asyncHandler(async function (_req, res) {
-const venues = await venue.findAll()
-    return res.json({venue});
+const venues = await Venue.findAll()
+    return res.json({venues});
   }));
 
 
 router.post('/', asyncHandler(async function (req, res) {
   const {name,address,city,state,zipCode,lat,lng} = req.body
-  const newVenue = await venue.create({
+  const newVenue = await Venue.create({
     name,
     address,
     city,
